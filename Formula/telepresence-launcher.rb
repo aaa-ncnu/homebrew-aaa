15d80896a913e35c7afe97909b57aa1042a7de17
Gary Morse <gary.morse@arizona.aaa.com>
2019-10-10 12:04:01 -0400

    Brew formula update for telepresence-launcher version v0.2.4

--- Formula/telepresence-launcher.rb
+++ Formula/telepresence-launcher.rb
@@ -2,9 +2,18 @@
 class TelepresenceLauncher < Formula
   desc "telepresence-launcher provides a guided interface to replace a Kubernetes deployment in an established K8s context in a consistent manner using Telepresence."
   homepage "https://github.com/aaa-ncnu/telepresence-launcher"
-  url "https://github.com/aaa-ncnu/telepresence-launcher/releases/download/v0.2.1/telepresence-launcher_0.2.1_MacOS_x86_64.tar.gz"
-  version "0.2.1"
-  sha256 "0bcc3fe9302bd3bedd4b5c1c55538c4977c72b7d76c727e39fb2be0a7c5ac8b4"
+  version "0.2.4"
+  bottle :unneeded
+
+  if OS.mac?
+    url "https://github.com/aaa-ncnu/telepresence-launcher/releases/download/v0.2.4/telepresence-launcher_0.2.4_MacOS_x86_64.tar.gz"
+    sha256 "f1dc33a17d08fb07ad01e4490dea75ba2858e7a9a84e50cc339234feb5d68624"
+  elsif OS.linux?
+    if Hardware::CPU.intel?
+      url "https://github.com/aaa-ncnu/telepresence-launcher/releases/download/v0.2.4/telepresence-launcher_0.2.4_Linux_x86_64.tar.gz"
+      sha256 "8ee5f29368e1c5c9ffe37864d912197c48b2338eefa41aee3846be8dcecff64a"
+    end
+  end
 
   def install
     bin.install "telepresence-launcher"
